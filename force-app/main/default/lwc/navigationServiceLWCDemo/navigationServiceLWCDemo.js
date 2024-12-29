65b848ae72ca0776e49e20a8973b44e0ab42d136
Jules Lee <jackconsolidated@gmail.com>
2024-12-29 14:57:38 +0800

    controller and controller extensions

--- force-app/main/default/lwc/navigationServiceLWCDemo/navigationServiceLWCDemo.js
+++ force-app/main/default/lwc/navigationServiceLWCDemo/navigationServiceLWCDemo.js
@@ -4,11 +4,6 @@ import { NavigationMixin } from 'lightning/navigation';
 export default class NavigationServiceLWCDemo extends NavigationMixin(LightningElement) {
     @api recordId;
 
-    /**
-     * Navigate to a new record page
-     * type: standard__recordPage
-     * attributes: recordId, objectApiName, actionName
-     */
     navigateToNewRecordPage() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -20,11 +15,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to an existing record page in edit mode
-     * type: standard__recordPage
-     * attributes: recordId, objectApiName, actionName
-     */
     navigateToEditRecordPage() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -36,11 +26,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to an existing record page in view mode
-     * type: standard__recordPage
-     * attributes: recordId, objectApiName, actionName
-     */
     navigateToViewRecordPage() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -52,12 +37,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to a list view page
-     * type: standard__objectPage
-     * attributes: objectApiName, actionName
-     * state: filterName
-     */
     navigateToRecentView() {
         this[NavigationMixin.Navigate]({
             "type": 'standard__objectPage',
@@ -71,12 +50,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }    
 
-    /**
-     * Navigate to a related list view page
-     * type: standard__recordRelationshipPage
-     * attributes: recordId, objectApiName, relationshipApiName, actionName
-     * state: filterName
-     */
     navigateToListViewPage() {
         this[NavigationMixin.Navigate]({
             "type": 'standard__recordRelationshipPage',
@@ -92,11 +65,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }   
 
-    /**
-     * Navigate to an object page
-     * type: standard__objectPage
-     * attributes: objectApiName, actionName
-     */
     navigateToAccObject() {
         this[NavigationMixin.Navigate]({
             "type": 'standard__objectPage',
@@ -107,11 +75,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to an object page
-     * type: standard__objectPage
-     * attributes: objectApiName, actionName
-     */
     navigateToConObject() { 
         this[NavigationMixin.Navigate]({
             "type": 'standard__objectPage',
@@ -122,11 +85,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to a web page
-     * type: standard__webPage
-     * attributes: url
-     */
     navigateToWebPage() {
         this[NavigationMixin.Navigate]({
             "type": 'standard__webPage',
@@ -136,11 +94,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to a named page
-     * type: standard__namedPage
-     * attributes: pageName
-     */
     navigateToHomePage() {
         this[NavigationMixin.Navigate]({
             "type": 'standard__namedPage',
@@ -150,11 +103,6 @@ export default class NavigationServiceLWCDemo extends NavigationMixin(LightningE
         });
     }
 
-    /**
-     * Navigate to a named page
-     * type: standard__namedPage
-     * attributes: pageName
-     */
     navigateToChatterHomePage() {
         this[NavigationMixin.Navigate]({
             "type": 'standard__namedPage',
